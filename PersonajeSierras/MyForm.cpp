e0d9121f30840100ddcfba901ba772047b972fb1
Lucero Abigail <91815464+LuceroAbigailOR@users.noreply.github.com>
2021-12-04 17:32:16 -0500

    Create MyForm.cpp

---
+++ PersonajeSierras/MyForm.cpp
@@ -0,0 +1,12 @@
+#include "MyForm.h"
+
+#include "MyForm.h"
+
+using namespace System;
+using namespace Windows::Forms;
+using namespace Ejercicio1W14;
+void main() {
+	//Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	Application::Run(gcnew Ejercicio1W14::MyForm);
+}
